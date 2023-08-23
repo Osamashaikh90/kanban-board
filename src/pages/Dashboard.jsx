b7a6e04043bb8318f333f54842745a700ef0511e
Osama <shaikhosama499@gmail.com>
2023-08-24 03:10:39 +0530

    feat(app): dashboard created

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CardList from "../components/card/cardList";
+const Dashboard = () => {
+  return (
+    <>
+      <div className="dashboard-container">
+        <CardList />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;

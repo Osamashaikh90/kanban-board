563543b951e4bba61f23e8a7fc92af2bd87ccf3f
Osama <shaikhosama499@gmail.com>
2023-08-23 23:39:34 +0530

    feat(app): top Nav created

---
+++ src/components/Navigation/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { GiSettingsKnobs } from "react-icons/gi";
+import { AiOutlineDown } from "react-icons/ai";
+import "./Navbar.css";
+const Navbar = () => {
+  const [display, setDisplay] = useState(false);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="display-section">
+          <button onClick={() => setDisplay(!display)}>
+            <GiSettingsKnobs className="icon" />
+            Display
+            <AiOutlineDown className="icon-1" />
+          </button>
+          {display && (
+            <div className="display-card">
+              <div style={{ display: "flex", gap: "4rem" }}>
+                <label style={{ width: "100%" }}>Grouping</label>
+                <select className="select-text">
+                  <option value="status">Display</option>
+                  <option value="user">User</option>
+                  <option value="priority">Priority</option>
+                </select>
+              </div>
+              <div style={{ display: "flex", gap: "4rem" }}>
+                <label style={{ width: "100%" }}>Ordering</label>
+                <select className="select-text">
+                  <option value="priority">priority</option>
+                  <option value="title">Title</option>
+                </select>
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;

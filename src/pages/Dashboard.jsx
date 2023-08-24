f7225b4b75e220d2af97ef7e45b1124034cf1b07
Osama <shaikhosama499@gmail.com>
2023-08-24 14:13:09 +0530

    feat(app): dashboard ui created

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import CardList from "../components/card/cardList";
+import { BsPencilSquare } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
+import { PiDotsThreeBold } from "react-icons/pi";
+import { useCardContext } from "../context/cardContext";
+import "./Dashboard.css";
+import { useEffect } from "react";
+import { useSelectedCardsContext } from "../context/selectedCardContext";
 const Dashboard = () => {
+  const { cards } = useCardContext();
+  const { selectedCards, selectCards } = useSelectedCardsContext();
+  console.log(cards);
+  useEffect(() => {
+    selectedCards("status", cards.tickets, "title");
+    console.log(selectCards);
+    console.log(cards);
+  }, [cards]);
   return (
     <>
       <div className="dashboard-container">
-        <CardList />
+        <div className="inner-container">
+          <div className="dashboard-heading ">
+            <div className="left" style={{ display: "flex", columnGap: "5px" }}>
+              <BsPencilSquare />
+              <div className="dashboard-profile">
+                <img
+                  className="dashboard-profile-pic"
+                  src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+                  alt="profile"
+                />
+              </div>
+              <span style={{ fontWeight: "500" }}>Osama</span>
+            </div>
+            <div
+              className="right"
+              style={{ display: "flex", columnGap: "5px" }}
+            >
+              <AiOutlinePlus />
+              <PiDotsThreeBold />
+            </div>
+          </div>
+          <CardList />
+        </div>
       </div>
     </>
   );
